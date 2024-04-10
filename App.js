919b9804abbf1cdcaaaec16d5336cb0aa41728c5
MarleyDouglas007 <marley.santos2308@gmail.com>
2024-04-10 12:38:23 -0300

    cor de fundo/borda de input

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aqui você pode adicionar a lógica para autenticar o usuário
+    // Por exemplo, fazer uma chamada para uma API
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        
+
+      <Text style={styles.title}>Faça o login</Text>
+
+      <Text style={styles.label}>Usuário</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <Text style={styles.label}>Usuário</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Entrar" onPress={handleLogin} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#AADDDD',
+    paddingHorizontal: 20,
   },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    borderRadius: 50,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+  },
+  label: {
+    right : 110,
+    marginBottom: 5,
+    color: '#000',
+    textAlign:'justify',
+  }
+
 });
+
+export default LoginScreen;
---
+++ App/index(login).js
@@ -0,0 +1,22 @@
+// index.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen'; // Supondo que sua tela de login esteja em LoginScreen.js
+import HomeScreen from './HomeScreen'; // Supondo que sua tela inicial esteja em HomeScreen.js
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
